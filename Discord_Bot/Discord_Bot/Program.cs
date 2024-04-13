e6dbe0fbd49c72eef5e31dd5c54a16bd62f2a5ce
Keernom <keernom.gdev@gmail.com>
2024-04-13 22:19:06 +0300

    feat: config finished

--- Discord_Bot/Discord_Bot/Program.cs
+++ Discord_Bot/Discord_Bot/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Discord_Bot
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Discord_Bot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DiscordSocketConfig config = new DiscordSocketConfig();
+
+            config.GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent;
+
+            var discordSocket = new DiscordSocketClient(config);
+
+            discordSocket.Log += Log;
+            await discordSocket.LoginAsync(TokenType.Bot, MySecrets.BOT_TOKEN);
+            await discordSocket.StartAsync();
+
+            discordSocket.MessageReceived += MessageReceived;
+            // Блокировка до закрытия программы
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private static async Task MessageReceived(SocketMessage m)
+        {
+            if (m.Author.IsBot) return;
+
+            switch (m.Content)
+            {
+                case "ping":
+                    await m.Channel.SendMessageAsync("pong");
+                    break;
+                default:
+                    Console.WriteLine($"{m.Author.Username}: {m.Content}");
+                    break;
+            }
         }
     }
 }
