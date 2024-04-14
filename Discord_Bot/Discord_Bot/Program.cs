806934e6be4e8c908827890d60543ece00d804ac
Keernom <keernom.gdev@gmail.com>
2024-04-15 01:17:59 +0300

    feat: bot chatting available

--- Discord_Bot/Discord_Bot/Program.cs
+++ Discord_Bot/Discord_Bot/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Discord_Bot.Entites;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Discord_Bot
 {
@@ -32,15 +35,56 @@ namespace Discord_Bot
         {
             if (m.Author.IsBot) return;
 
-            switch (m.Content)
+            var answer = await GetAnswer(m.Content);
+            await Console.Out.WriteLineAsync(answer);
+            var a = JsonConvert.DeserializeObject<YaGPTResult>(answer);
+            string answerText = a.Result.Alternatives[0].Message.text;
+            
+            await m.Channel.SendMessageAsync(answerText);
+        }
+
+        public static async Task<string> GetAnswer(string userMessage)
+        {
+            var opts = new CompletionOptions()
             {
-                case "ping":
-                    await m.Channel.SendMessageAsync("pong");
-                    break;
-                default:
-                    Console.WriteLine($"{m.Author.Username}: {m.Content}");
-                    break;
-            }
+                stream = false,
+                temperature = 1f,
+                maxTokens = 150
+            };
+
+            List<Message> messages = new List<Message>()
+            {
+                //new Message()
+                //{
+                //    role = "system",
+                //    text = "Ты финансовый консультант"
+                //},
+                new Message()
+                {
+                    role = "user",
+                    text = userMessage
+                }
+            };
+
+            string modelUri = $"gpt://{MySecrets.FOLDER_ID}/yandexgpt";
+
+            var body = new RequestBody() 
+            { 
+                modelUri = modelUri,
+                completionOptions = opts,
+                messages = messages
+            };
+
+            string jsonBody = JsonConvert.SerializeObject(body);
+
+            var client = new RestClient("https://llm.api.cloud.yandex.net/foundationModels/v1/completion");
+            var request = new RestSharp.RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", $"Api-Key {MySecrets.API_KEY}");
+            request.AddBody(jsonBody);
+
+            var response = await client.ExecutePostAsync<object>(request);
+            return response.Content;
         }
     }
 }
